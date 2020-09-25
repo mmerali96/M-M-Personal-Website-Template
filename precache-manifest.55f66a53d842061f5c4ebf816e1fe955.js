d79617cb75827a9dff691eaeecec7156cc7d81b9
Mehdi Merali <mmerali@Mehdis-MacBook-Pro-2.local>
2020-09-24 23:14:43 -0400

    Updates

---
+++ precache-manifest.55f66a53d842061f5c4ebf816e1fe955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b565bc3a34e8b2ab13f2966e6efb2d1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d277350fa09a4dea909c",
+    "url": "/static/css/main.783e31d7.chunk.css"
+  },
+  {
+    "revision": "e92812ce829feb78aaf4",
+    "url": "/static/js/2.bf1c4fa2.chunk.js"
+  },
+  {
+    "revision": "6734d0bfe367c40a5b2a23ab2f00b933",
+    "url": "/static/js/2.bf1c4fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d277350fa09a4dea909c",
+    "url": "/static/js/main.7dfccffd.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "35a97675d491fd971d1bd2352b5e3c86",
+    "url": "/static/media/Resume.35a97675.pdf"
+  }
+]);
